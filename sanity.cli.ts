d588e6257c1cb125c2b268dbb29ec4df63aea954
Chloe <chloe@ChloedeMacBook-Air.local>
2026-06-04 15:54:21 +0800

    Save Sanity Studio host

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,6 +1,7 @@
 import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
+  studioHost: 'chloe-art-of-moving',
   api: {
     projectId: process.env.SANITY_STUDIO_PROJECT_ID || 'YOUR_SANITY_PROJECT_ID',
     dataset: process.env.SANITY_STUDIO_DATASET || 'production'
